aac18efdd9fed865440de9126c5c450ee750ed05
Claude Lehmann <e.dualc@hotmail.com>
2024-10-06 15:34:40 +0200

    download data script

---
+++ data/download_atai_files.sh
@@ -0,0 +1,4 @@
+wget -r --no-parent -nH --cut-dirs=4 https://files.ifi.uzh.ch/ddis/teaching/ATAI2024/dataset/
+unzip ddis-graph-embeddings.zip
+unzip ddis-movie-graph.nt.zip
+unzip ddis-movie-graph.tsv.zip
