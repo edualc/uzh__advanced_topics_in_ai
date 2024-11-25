77ff260778ec23ad77d7de48712307e13597406f
Melih Serin <melihsrnn@gmail.com>
2024-11-25 12:34:20 +0100

    minor change

--- src/recommendation.py
+++ src/recommendation.py
@@ -20,51 +20,51 @@ class RecommendationBot:
         self.relation_emb = relation_emb
 
     def get_recommendation(self, input):
-        # try:
-        ner_results=self.bot_ner._ner_results(input)
-        if len(ner_results)==0:
-            genre_search = [(i,i+" film") for i in input.replace(".","").split(" ")]
-            matched_genres = []
-            for word,word_film in genre_search:
-                if word in self.genres.keys():
-                    matched_genres.append((word, self.genres[word]))
-                if word_film in self.genres.keys():
-                    matched_genres.append((word_film, self.genres[word_film]))
-            recommendation = self.recommend_genre_embed([url for label, url in matched_genres])
-            if recommendation[0] is None:
-                return "Sorry, I cannot recommend you a movie based on your query. The reasons might be that I do not know the movies you mentioned or there is a minor problem with the format of your input. You might want to re-check and/or rephrase your sentence. I will be waiting here. "
-            genres_matched = ""
-            for matched in list(dict.fromkeys([label.replace(" film","") for label,url in matched_genres])):
-                genres_matched +=matched + ", "
-            genres_matched = genres_matched.strip()[:-1]
-            return f"If you like {genres_matched} movies, I would recommend you watch {recommendation[0][1]}."
-            
-        entities = self.bot_ner.entity_extraction(ner_results, input)
-        entities_matched = [self.bot_ner.match_things(self.nodes, ent) for ent in entities]
-        urls_entities = [url for ent,url in entities_matched]
         try:
-            pub_dates = sorted(list(dict.fromkeys([[str(o)[:3] for s,p,o in self.g.triples((URIRef(url), URIRef(self.predicates["publication date"]), None))][0]
-                            for url in urls_entities])))
-        except:
-            pub_dates=[]
-        recommendation = self.recommend_genre_embed(urls_entities)
-        if recommendation[0] is None:
-            return "Sorry, I cannot recommend you a movie based on your query. The reasons might be that I do not know the movies you mentioned or there is a minor problem with the format of your input. You might want to re-check and/or rephrase your sentence. I will be waiting here "
-        genres_matched = ""
-        genres_checked = []
-        for i in range(len(recommendation[1])):
-            if recommendation[1][i][1].split(" ")[0]=="film":
-                continue
-            genres_checked.append(recommendation[1][i][1].replace(' film',''))
-        genres_matched += ", ".join(genres_checked)
-        genres_matched = genres_matched
-        if len(pub_dates)==2:
-            genres_matched += " from around " + pub_dates[0] +"0s or " + pub_dates[1] + "0s"
-        elif len(pub_dates)==1:
-            genres_matched += " from around " + pub_dates[0] +"0s"
-        return f"Based on what you like, I would recommend you watching movie with the genres {genres_matched} such as {recommendation[0][1]}."
-        # except Exception as e:
-        #     return "Sorry, I cannot recommend you a movie based on your query. The reasons might be that I do not know the movies you mentioned or there is a minor problem with the format of your input. You might want to re-check and/or rephrase your sentence. I will be waiting here. "
+            ner_results=self.bot_ner._ner_results(input)
+            if len(ner_results)==0:
+                genre_search = [(i,i+" film") for i in input.replace(".","").split(" ")]
+                matched_genres = []
+                for word,word_film in genre_search:
+                    if word in self.genres.keys():
+                        matched_genres.append((word, self.genres[word]))
+                    if word_film in self.genres.keys():
+                        matched_genres.append((word_film, self.genres[word_film]))
+                recommendation = self.recommend_genre_embed([url for label, url in matched_genres])
+                if recommendation[0] is None:
+                    return "Sorry, I cannot recommend you a movie based on your query. The reasons might be that I do not know the movies you mentioned or there is a minor problem with the format of your input. You might want to re-check and/or rephrase your sentence. I will be waiting here. "
+                genres_matched = ""
+                for matched in list(dict.fromkeys([label.replace(" film","") for label,url in matched_genres])):
+                    genres_matched +=matched + ", "
+                genres_matched = genres_matched.strip()[:-1]
+                return f"If you like {genres_matched} movies, I would recommend you watch {recommendation[0][1]}."
+                
+            entities = self.bot_ner.entity_extraction(ner_results, input)
+            entities_matched = [self.bot_ner.match_things(self.nodes, ent) for ent in entities]
+            urls_entities = [url for ent,url in entities_matched]
+            try:
+                pub_dates = sorted(list(dict.fromkeys([[str(o)[:3] for s,p,o in self.g.triples((URIRef(url), URIRef(self.predicates["publication date"]), None))][0]
+                                for url in urls_entities])))
+            except:
+                pub_dates=[]
+            recommendation = self.recommend_genre_embed(urls_entities)
+            if recommendation[0] is None:
+                return "Sorry, I cannot recommend you a movie based on your query. The reasons might be that I do not know the movies you mentioned or there is a minor problem with the format of your input. You might want to re-check and/or rephrase your sentence. I will be waiting here "
+            genres_matched = ""
+            genres_checked = []
+            for i in range(len(recommendation[1])):
+                if recommendation[1][i][1].split(" ")[0]=="film":
+                    continue
+                genres_checked.append(recommendation[1][i][1].replace(' film',''))
+            genres_matched += ", ".join(genres_checked)
+            genres_matched = genres_matched
+            if len(pub_dates)==2:
+                genres_matched += " from around " + pub_dates[0] +"0s or " + pub_dates[1] + "0s"
+            elif len(pub_dates)==1:
+                genres_matched += " from around " + pub_dates[0] +"0s"
+            return f"Based on what you like, I would recommend you watching movie with the genres {genres_matched} such as {recommendation[0][1]}."
+        except Exception as e:
+            return "Sorry, I cannot recommend you a movie based on your query. The reasons might be that I do not know the movies you mentioned or there is a minor problem with the format of your input. You might want to re-check and/or rephrase your sentence. I will be waiting here. "
 
     def match_list_items(self, list_of_lists):
         print(list_of_lists)
@@ -101,8 +101,8 @@ class RecommendationBot:
             idx_movie.append(pairwise_distances(head.reshape(1, -1), self.entity_emb).reshape(-1).argmin())
             dist_genre += pairwise_distances(lhs.reshape(1, -1), self.entity_emb).reshape(-1)
 
-        if len(objects_movie) == 0:
-            return None, None
+        # if len(objects_movie) == 0:
+        #     return None, None
         
         # find most plausible entities
         matched_genres = self.match_list_items(objects_movie)
@@ -132,8 +132,8 @@ class RecommendationBot:
                     most_plausible_genre.append((str(self.id2ent[url]), url))
                 except KeyError:
                     continue
-            if len(most_plausible_genre)==0:
-                return None, None
+            # if len(most_plausible_genre)==0:
+            #     return None, None
         else:
             most_plausible_genre = [(str(self.id2ent[idx]), self.url2nodes[self.id2ent[idx]])
                 for idx in most_likely_genre[:3]]
